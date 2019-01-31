f73c7815a7f866492f6c7be8235334e7be27f2d7
e-barr <31676765+e-barr@users.noreply.github.com>
2019-01-30 21:23:44 -0600

    set up redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+
+    const resp = await jsonPlaceholder.get('/posts')
+    
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: resp
+    })
+}
