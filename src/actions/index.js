440343f685c526dd9cefdcdab24f55e794c1f337
e-barr <31676765+e-barr@users.noreply.github.com>
2019-02-01 10:08:42 -0600

    reduce load time

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async (dispatch , getState)=> {
+    await dispatch(fetchPosts())
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
+}
 
 export const fetchPosts = () => async dispatch => {
 
@@ -10,7 +21,7 @@ export const fetchPosts = () => async dispatch => {
     })
 }
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => async dispatch => {
     const resp = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({
@@ -18,3 +29,15 @@ export const fetchUser = (id) => async dispatch => {
         payload: resp.data
     })
 }
+// export const fetchUser = (id) => async dispatch => {
+//     _fetchUser(id, dispatch)
+// }
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const resp = await jsonPlaceholder.get(`/users/${id}`)
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: resp.data
+//     })
+// })
