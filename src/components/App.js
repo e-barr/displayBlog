b7e78ca67c86e277eb0e7fa593f3d04b9c286df4
e-barr <31676765+e-barr@users.noreply.github.com>
2019-02-01 10:45:33 -0600

    add UserInfoCardComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PostList from './PostList'
+import UserInfoCard from './UserInfoCard'
+import './App.css'
 
 const App = () => {
     return (
         <div className="ui container">
             <PostList />
+            <UserInfoCard />
         </div>
     )
 }
---
+++ src/components/UserInfoCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserInfoCard extends Component {
+    render() {
+        return(
+            <div>
+                UserInfoCard
+            </div>
+        )
+    }
+}
+
+export default UserInfoCard
