30b526e6207a262418c9e2d14efb022e4edfb6e8
e-barr <31676765+e-barr@users.noreply.github.com>
2019-02-01 14:48:18 -0600

    styling added

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPostsAndUsers } from '../actions'
 import UserHeader from './UserHeader'
+import './PostList.css'
 
 class PostList extends Component {
     componentDidMount() {
@@ -11,13 +12,14 @@ class PostList extends Component {
     renderList() {
         return this.props.posts.map(post => {
             return (
-                <div className="item" key={post.id} >
-                    <i className="large middle aligned icon user" />
+                <div className="item item-details" key={post.id} >
+                    <i className="large middle aligned icon user" style={{ padding: '0 10px 0 10px'}} />
                         <div className="content">
                             <div className="description">
                                 <h2>{post.title}</h2>
                                 <p>{post.body}</p>
                             </div>
+                            <br></br>
                             <UserHeader userId={post.userId} />
                         </div>
                 </div>
--- src/components/UserInfoCard.js
+++ src/components/UserInfoCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deselectedUser } from '../actions'
+import './UserInfoCard.css'
 
 class UserInfoCard extends Component {
     render() {
@@ -10,10 +11,34 @@ class UserInfoCard extends Component {
         }
 
         console.log(this.props)
+        const { street, suite, city, zipcode, geo } = user.address
+        const { name, catchPhrase, bs } = user.company
 
         return(
-            <div onClick={this.props.deselectedUser}>
-                {user.name}
+            <div className="user-info-card-container">
+                <div>
+                    <button onClick={this.props.deselectedUser} style={{ float: 'right'}}>X</button>
+                </div>
+                <h1>{user.username}</h1>
+                <h3>aka {user.name}</h3>
+                <p>contact</p>
+                <ul>
+                    <li><i class="envelope outline icon"></i>
+                    <a href="mailto:">{user.email}</a>
+                    </li>
+                    <li>
+                    <i class="map marker icon"></i>{`${street} ${suite} ${city} ${zipcode}`}
+                    </li>
+                    <li>geocoordinates: {`${geo.lat} ${geo.lng}`}</li>
+                </ul>
+
+                <p>company</p>
+                <h3>{name}</h3>
+                <p>
+                    {catchPhrase}<br></br>
+                    {bs}
+                </p>
+
             </div>
         )
     }
